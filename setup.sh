b7b92b9719712f1f05631d238f9a228899ebab09
NightWolf92 <rcurran92@gmail.com>
2022-01-03 16:50:08 -0500

    create setup.sh
    This can be used to setup prometheus accounts

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo mkdir -p /etc/prometheus/
+sudo cp prometheus.yml /etc/prometheus/
+sudo cp prometheus /usr/local/bin/ -r
+sudo chown -R prometheus:prometheus /etc/prometheus/
+cp prometheus.service /etc/systemd/system/
