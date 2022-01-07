ea786e83ce881267f2b08c2ba1389b16754f3e05
NightWolf92 <rcurran92@gmail.com>
2022-01-07 15:29:21 -0500

    Update livepeer_Setup.sh
    Fix grafana repo

--- livepeer_Setup.sh
+++ livepeer_Setup.sh
@@ -39,6 +39,7 @@ sudo cp prometheus.service /etc/systemd/system/
 #Download and install grafana
 sudo apt-get install -y apt-transport-https
 sudo apt-get install -y software-properties-common wget
+echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install grafana-enterprise -y
