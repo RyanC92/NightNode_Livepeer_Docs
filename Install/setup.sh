ec48f07772050ac0e53065a4c6fdb50e26f0fde0
NightWolf92 <rcurran92@gmail.com>
2022-01-12 18:06:23 -0500

    Update setup.sh

--- Install/setup.sh
+++ Install/setup.sh
@@ -3,76 +3,55 @@
 #move to home directory
 cd $HOME
 
+#install sudo (for minimal installs
+apt-get update -y && apt-get install sudo  apt-transport-https software-properties-common wget -y
+
 #Download Prometheus, grafana and necessary dependencies
 
 wget https://github.com/prometheus/prometheus/releases/download/v2.32.1/prometheus-2.32.1.linux-amd64.tar.gz
 wget https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/prometheus.yml
-wget https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/livepeer.sh
 wget https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/livepeer.service
 wget https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/prometheus.service
-wget https://github.com/livepeer/go-livepeer/releases/download/v0.5.23/livepeer-linux-amd64.tar.gz
 wget https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/orchestrator.target
-
-#set livepeer.sh to execute
-chmod +x ./livepeer.sh
-
-#Prometheus Setup 
-tar -xvzf prometheus-2.32.1.linux-amd64.tar.gz
-# this is to rename it if you want.
-mv prometheus-2.32.1.linux-amd64 prometheus
-
-#check for updates and install sudo
-apt-get update && apt-get -y install sudo
-
-#Create Prometheus user account, copy the yml file, setup the service and take ownership.
-sudo useradd --no-create-home --shell /bin/false prometheus
-sudo useradd --no-create-home --shell /bin/false livepeer
-sudo mkdir -p /etc/prometheus/
-sudo cp prometheus.yml /etc/prometheus/
-sudo cp prometheus /usr/local/bin/ -r
-sudo chown -R prometheus:prometheus /etc/prometheus/
-sudo cp prometheus.service /etc/systemd/system/
-
-#setup the services for Prometheus
-#sudo systemctl daemon-reload
-#sudo systemctl start prometheus.service
-#sudo systemctl enable prometheus.service
-#sudo systemctl status prometheus.service
-
-#Download and install grafana
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
-echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-sudo apt-get update -y
-sudo apt-get install grafana-enterprise -y
-
-#Start the grafana server with systemd
-#sudo systemctl daemon-reload
-#sudo systemctl start grafana-server
-#sudo systemctl enable grafana-server.service
-#sudo systemctl status grafana-server
+wget https://github.com/livepeer/go-livepeer/releases/download/v0.5.23/livepeer-linux-amd64.tar.gz
 
 #Install Livepeer
 tar -xvzf livepeer-linux-amd64.tar.gz
 # this is to rename it if you want.
 mv livepeer-linux-amd64 livepeer
 
-sudo cp livepeer /usr/local/bin/ -r
-sudo cp livepeer.sh /usr/local/bin/livepeer/
-sudo cp livepeer.service /etc/systemd/system/
-sudo cp orchestrator.target /etc/systemd/system
+wget -P livepeer/ https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/Install/livepeer_transcoder.conf
+wget -P livepeer/ https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/Install/livepeer_orchestrator.conf
+wget -P livepeer/ https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/livepeer.sh
 
+#set livepeer.sh to execute
+chmod +x livepeer/livepeer.sh
 
-#setup the services for livepeer
-#sudo systemctl daemon-reload
-#sudo systemctl start livepeer.service
-#sudo systemctl enable livepeer.service
-#sudo systemctl enable orchestrator.target
-#sudo systemctl status prometheus.service
+sudo mv livepeer /usr/local/bin/ -r
+sudo mv livepeer.service /etc/systemd/system/
+sudo mv orchestrator.target /etc/systemd/system
 
+#Prometheus Setup 
+tar -xvzf prometheus-2.32.1.linux-amd64.tar.gz
+# this is to rename it if you want.
+mv prometheus-2.32.1.linux-amd64 prometheus
+
+#Create Prometheus user account, copy the yml file, setup the service and take ownership.
+sudo useradd --no-create-home --shell /bin/false prometheus
+sudo useradd --no-create-home --shell /bin/false livepeer
+sudo mkdir -p /etc/prometheus/
+sudo mv prometheus.yml /etc/prometheus/
+sudo mv prometheus /usr/local/bin/ -r
+sudo chown -R prometheus:prometheus /etc/prometheus/
+sudo mv prometheus.service /etc/systemd/system/
+
+#Download and install grafana
+echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install grafana-enterprise -y
 
 #Cleanup
 cd $HOME
 
-rm livepeer-linux-amd64.tar.gz orchestrator.target  prometheus-2.32.1.linux-amd64.tar.gz prometheus.service livepeer livepeer.service livepeer.sh prometheus prometheus.service prometheus.yml -r
+rm livepeer-linux-amd64.tar.gz prometheus-2.32.1.linux-amd64.tar.gz prometheus.service -r
