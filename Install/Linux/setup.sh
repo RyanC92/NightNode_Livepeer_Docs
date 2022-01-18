9261f9cff777395441312c979d64c7585e2f77c8
NightWolf92 <rcurran92@gmail.com>
2022-01-18 16:12:07 -0500

    Update setup.sh

--- Install/Linux/setup.sh
+++ Install/Linux/setup.sh
@@ -63,6 +63,9 @@ wget -N -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install grafana-enterprise -y
 
+#downloading part 2 to enable and start the services, edit this file if you want to start a transcoder service only.
+wget -N https://github.com/NightWolf92/NightNode_Livepeer_Docs/raw/main/Install/Linux/Livepeer_Setup_2_(Services).sh
+
 #Cleanup
 cd $HOME
 rm $HOME/livepeersetup/ -r
