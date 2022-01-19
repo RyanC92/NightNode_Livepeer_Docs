1f2853c620c8b5f1a98b1537258fbc35d4c79615
NightWolf92 <rcurran92@gmail.com>
2022-01-19 09:39:08 -0500

    Update Livepeer_Setup_Services.sh

--- Install/Linux/Livepeer_Setup_Services.sh
+++ Install/Linux/Livepeer_Setup_Services.sh
@@ -3,4 +3,4 @@
 #Start and enable all the services once the configurations have been made
 sudo systemctl daemon-reload
 sudo systemctl start prometheus.service grafana-server livepeer.service orchestrator.target
-sudo systemctl enable prometheus.service grafana-server.service livepeer.service orchestrator.target
+sudo systemctl enable prometheus.service grafana-server livepeer.service orchestrator.target
