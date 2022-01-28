c224ebb2032424edd8b425efd40a86fbc3be7618
NightWolf92 <rcurran92@gmail.com>
2022-01-28 12:08:56 -0500

    Update Data Migration.sh

--- Migration/Data Migration.sh
+++ Migration/Data Migration.sh
@@ -6,9 +6,9 @@ mkdir migration
 sudo systemctl stop prometheus.service
 sudo systemctl stop livepeer.service
 
-cp /etc/prometheus/* migration/prometheus/
-cp /var/lib/livepeer/.lpData/ /migraiton/.lpData/
-cp /etc/livepeer/ /migration/livepeer/
-cp /home/livepeer/.lpData/ /migration/home/.lpData/
+cp /etc/prometheus/ migration/prometheus/ -r
+cp /var/lib/livepeer/.lpData/ /migraiton/.lpData -r
+cp /etc/livepeer/ /migration/livepeer/ -r
+cp /home/livepeer/.lpData/ /migration/home/.lpData/ -r
 
 sudo tar -czvf livepeer_migration.tar.gz /migration/
