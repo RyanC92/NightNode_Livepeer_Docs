c5e2a8d0508558227626646ef01b11f11e59bc77
NightWolf92 <rcurran92@gmail.com>
2022-04-15 15:50:20 -0400

    Live exe setup

--- Install/Windows/Orchestrator.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:Restart
-
-TITLE Orchestrator
-
-.\livepeer.exe -config livepeer_orchestrator.conf
-
-goto Restart
-pause
--- Install/Windows/Run Benchmark.bat
+++
@@ -1,3 +0,0 @@
-.\livepeer_bench.exe -in bbb/source.m3u8 -transcodingOptions .\transcodingOptions.json -nvidia 0 -concurrentSessions 20
-
-pause
